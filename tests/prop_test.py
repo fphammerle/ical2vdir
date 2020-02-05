c187fab84e9c594eef96e44f5030511b5794d6bc
Fabian Peter Hammerle <fabian@hammerle.me>
2020-02-05 20:15:52 +0100

    some more unit tests for _event_prop_equal

--- tests/prop_test.py
+++ tests/prop_test.py
@@ -1,5 +1,7 @@
+import datetime
+
 import pytest
-from icalendar.prop import vCalAddress, vInt, vText
+from icalendar.prop import vCalAddress, vDDDLists, vDDDTypes, vInt, vRecur, vText
 
 from ics2vdir import _event_prop_equal
 
@@ -21,6 +23,81 @@ from ics2vdir import _event_prop_equal
         (
             vCalAddress("mailto:someone@somewhere.com"),
             vCalAddress("mailto:someelse@somewhere.com"),
+            False,
+        ),
+        (vRecur(FREQ="WEEKLY", COUNT=21), vRecur(FREQ="WEEKLY", COUNT=21), True,),
+        (vRecur(FREQ="WEEKLY", COUNT=21), vRecur(FREQ="WEEKLY", COUNT=42), False,),
+        (
+            vDDDTypes(
+                datetime.datetime(2012, 7, 3, 16, 39, 2, tzinfo=datetime.timezone.utc)
+            ),
+            vDDDTypes(
+                datetime.datetime(2012, 7, 3, 16, 39, 2, tzinfo=datetime.timezone.utc)
+            ),
+            True,
+        ),
+        (
+            vDDDTypes(
+                datetime.datetime(2012, 7, 3, 16, 39, 3, tzinfo=datetime.timezone.utc)
+            ),
+            vDDDTypes(
+                datetime.datetime(2012, 7, 3, 16, 39, 2, tzinfo=datetime.timezone.utc)
+            ),
+            False,
+        ),
+        (
+            vDDDLists(
+                [datetime.datetime(2020, 2, 5, 20, 0, tzinfo=datetime.timezone.utc)]
+            ),
+            vDDDLists(
+                [datetime.datetime(2020, 2, 5, 20, 0, tzinfo=datetime.timezone.utc)]
+            ),
+            True,
+        ),
+        (
+            vDDDLists(
+                [
+                    datetime.datetime(2020, 2, 5, 20, 0, tzinfo=datetime.timezone.utc),
+                    datetime.datetime(2020, 2, 5, 20, 5, tzinfo=datetime.timezone.utc),
+                ]
+            ),
+            vDDDLists(
+                [
+                    datetime.datetime(2020, 2, 5, 20, 0, tzinfo=datetime.timezone.utc),
+                    datetime.datetime(2020, 2, 5, 20, 5, tzinfo=datetime.timezone.utc),
+                ]
+            ),
+            True,
+        ),
+        (
+            vDDDLists(
+                [
+                    datetime.datetime(2020, 2, 5, 20, 0, tzinfo=datetime.timezone.utc),
+                    datetime.datetime(2020, 2, 5, 20, 5, tzinfo=datetime.timezone.utc),
+                ]
+            ),
+            vDDDLists(
+                [
+                    datetime.datetime(2020, 2, 5, 20, 0, tzinfo=datetime.timezone.utc),
+                    datetime.datetime(2020, 2, 5, 20, 7, tzinfo=datetime.timezone.utc),
+                ]
+            ),
+            False,
+        ),
+        (
+            vDDDLists(
+                [
+                    datetime.datetime(2020, 2, 5, 20, 0, tzinfo=datetime.timezone.utc),
+                    datetime.datetime(2020, 2, 5, 20, 5, tzinfo=datetime.timezone.utc),
+                ]
+            ),
+            vDDDLists(
+                [
+                    datetime.datetime(2020, 2, 5, 20, 0, tzinfo=datetime.timezone.utc),
+                    datetime.datetime(2020, 2, 5, 20, 5, tzinfo=datetime.timezone.utc),
+                    datetime.datetime(2020, 2, 5, 20, 7, tzinfo=datetime.timezone.utc),
+                ]
+            ),
             False,
         ),
     ],
