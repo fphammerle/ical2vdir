0bd9794e74e789362d4df7d6d5271ebe5bb7bd5d
Fabian Peter Hammerle <fabian@hammerle.me>
2025-10-17 08:52:17 +0200

    test against python3.12–13 & declare compatibility

--- setup.py
+++ setup.py
@@ -44,6 +44,8 @@ setuptools.setup(
         # .github/workflows/python.yml
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Utilities",
     ],
     entry_points={
